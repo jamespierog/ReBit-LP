23917c654756a2cf8c962cf05b3d38ea45a91b3d
James Pierog <james.pierog@gmail.com>
2022-11-15 17:59:39 +0800

    tried to fix navbar bug

--- components/navbar.js
+++ components/navbar.js
@@ -59,8 +59,8 @@ export default function Navbar() {
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
-                      <Link key={index} href="/">
-                        <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none dark:focus:bg-trueGray-700">
+                            <Link key={index} href={`/#${item}`}>
+                            <a href={`/#${item}`} className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none dark:focus:bg-trueGray-700">
                           {item}
                         </a>
                       </Link>
